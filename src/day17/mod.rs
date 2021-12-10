1b3b1558d8ec5028f0ab5ef6b0e577a549dea45f
dementati <ztargaser@gmail.com>
2021-12-10 08:49:38 +0100

    Improve day 17

--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -1,9 +1,4 @@
-use std::collections::HashSet;
-use std::collections::BTreeSet;
-use std::collections::HashMap;
-
-type Cache = HashMap<(i128, Set), HashSet<Set>>;
-type Set = BTreeSet<(usize, i128)>;
+use itertools::Itertools;
 
 pub fn solver(star: u8) -> fn(String) -> i128 {
     match star {
@@ -14,61 +9,42 @@ pub fn solver(star: u8) -> fn(String) -> i128 {
 }
 
 fn star1(input: String) -> i128 {
-    combinations(&input).len() as i128
-}
-
-fn combinations(input: &str) -> HashSet<Set> {
-    let c: Vec<(usize, i128)> = input.split_whitespace()
+    let c: Vec<i128> = input.split_whitespace()
         .map(|s| s.parse().unwrap())
-        .enumerate()
         .collect();
-    let mut cache = Cache::new();
-    fit(c, 150, Set::new(), &mut cache)
-}
 
-fn fit(
-    c: Vec<(usize, i128)>, 
-    n: i128, 
-    path: Set, 
-    cache: &mut Cache,
-) -> HashSet<Set> {
-    if  n == 0 {
-        let mut result = HashSet::new();
-        result.insert(path.clone());
-        return result;
-    }
-
-    let key = (n, path.clone());
-    if cache.contains_key(&key) {
-        return cache[&key].clone();
-    }
-
-    c.iter()
-        .filter(|(_, v)| v <= &n)
-        .map(|(i, v)| {
-            let c_: Vec<_> = c.iter()
-                .filter(|(j, _)| i != j)
-                .cloned()
-                .collect();
-            let n_ = n - v;
-            let mut path_ = path.clone();
-            path_.insert((*i, *v));
-            let result = fit(c_, n_, path_, cache);
-            cache.insert(key.clone(), result.clone());
-            result
-        })
-        .fold(HashSet::new(), |a, b| a.union(&b).cloned().collect())
+    (1..=c.len())
+        .flat_map(|i| 
+            c.iter().combinations(i)
+                .map(|c| c.iter().copied().sum::<i128>())
+        )
+        .filter(|&s| s == 150)
+        .count() as i128
 }
 
 fn star2(input: String) -> i128 {
-    let result = combinations(&input);
+    let c: Vec<i128> = input.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let result: Vec<Vec<i128>> = (1..=c.len())
+        .flat_map(|i| 
+            c.iter().combinations(i)
+                .map(|c| 
+                    c.into_iter()
+                        .copied()
+                        .collect::<Vec<_>>()
+                )
+        )
+        .filter(|s| s.iter().sum::<i128>() == 150)
+        .collect();
 
     let m = result.iter()
-        .map(|s| s.len())
+        .map(|p| p.len())
         .min()
         .unwrap();
 
     result.iter()
-        .filter(|s| s.len() == m)
+        .filter(|p| p.len() == m)
         .count() as i128
 }
